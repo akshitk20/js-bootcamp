1292bef7d7726408f0eb7d8157259b5cf13a2195
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-03 21:05:28 +0530

    added variable examples

---
+++ code.js
@@ -0,0 +1,73 @@
+console.log("Hello world!");
+let score = 0;
+console.log(score);
+score = 6;
+console.log(score);
+// cannot declare variable twice
+
+let customers; 
+console.log(customers); // undefined
+
+customers = 2;
+console.log(customers);
+
+const diameter = 5;
+console.log(5); // constant
+
+var score2 = 10 ; // old way of declaring variable
+console.log(score2); 
+
+let num = 41.3
+console.log(num);
+
+let greetings = `Hello there`
+console.log(greetings);
+
+let isStudent = false;
+console.log(isStudent);
+
+let myCustomers = null;
+console.log(myCustomers);
+
+let name = "Sherlock";
+let isHome = false;
+let age = 20;
+let address;
+let city = null;
+
+console.log(typeof city);
+
+let s = 6;
+s = s + 1;
+s = (s/2) * 10;
+console.log(s);
+
+let bonus = 10;
+s = s + bonus;
+console.log(s);
+
+let i = 10;
+console.log(typeof i);
+
+let f1 = 10.2
+console.log(typeof f1);
+
+let f2 = parseFloat("3.141");
+console.log(f2);
+
+let z = parseInt("90201");
+console.log(z);
+
+let var1 = 1/0; // print Infinity no run time error is thrown
+console.log(var1);
+
+let weird = 10 / "pizza";
+console.log(weird);
+
+console.log(Math.PI);
+
+let rand = Math.floor(Math.random() * 10) ;
+console.log(rand);
+
+let rand1 = Math.round(Math.random() * 10) ;
+console.log(rand1);
