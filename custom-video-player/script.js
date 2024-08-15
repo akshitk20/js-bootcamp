c7ca2fba6d9e995798cc2fdf6a4783f79712350b
Akshit Khatri <akshitkhatri.20@gmail.com>
2024-08-15 18:40:11 +0530

    completed script.js

---
+++ custom-video-player/script.js
@@ -0,0 +1,61 @@
+// GET our elements
+const player = document.querySelector('.player');
+const video = player.querySelector('.viewer');
+const progress = player.querySelector('.progress');
+const progressBar = player.querySelector('.progress__filled');
+const toggle = player.querySelector('.toggle');
+const skipButtons = player.querySelectorAll('[data-skip]');
+const ranges = player.querySelectorAll('.player__slider');
+
+// build our functions
+function togglePlay() {
+    //console.dir(video);
+    const method = video.paused ? 'play' : 'pause';
+    video[method](); // same as below if else
+    // if (video.paused) {
+    //     video.play();
+    // } else {
+    //     video.pause();
+    // }
+
+}
+function updateButton() {
+    console.log("update the button");
+    const icon = this.paused ? '►' : '❚ ❚';
+    toggle.textContent = icon;
+}
+
+function skip() {
+    console.log(this.dataset.skip);
+    video.currentTime += parseFloat(this.dataset.skip);    
+}
+
+function handleRangeUpdate() {
+    video[this.name] = this.value;
+    console.log(this.name);
+    console.log(this.value);
+}
+
+function handleProgress() {
+    const percent = (video.currentTime / video.duration) * 100;
+    //console.log(percent);
+    progressBar.style.flexBasis = `${percent}%`;
+
+}
+
+function scrub(e) {
+    const scrubTime = (e.offsetX / progress.offsetWidth) * video.duration;
+    video.currentTime = scrubTime;
+    console.log(e);
+}
+
+// hook up event listeners
+video.addEventListener('click', togglePlay);
+video.addEventListener('play', updateButton);
+video.addEventListener('pause', updateButton);
+video.addEventListener('timeupdate', handleProgress);
+
+toggle.addEventListener('click', togglePlay);
+skipButtons.forEach(button => button.addEventListener('click', skip));
+ranges.forEach(range => range.addEventListener('change', handleRangeUpdate));
+progress.addEventListener('click', scrub);
